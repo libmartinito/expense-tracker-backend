7dd6d17b5735d90eac49f6e83b0c6eb00d141b3f
lib <lib.martinito@pm.me>
2024-09-25 04:16:28 +0800

    feat: add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
   has_secure_token :auth_token
+
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
 end
