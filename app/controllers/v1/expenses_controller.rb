709516268c4a872cde5edb905d0805a83cfe563d
lib <lib.martinito@pm.me>
2024-09-24 20:22:13 +0800

    feat: update links and expense params

--- app/controllers/v1/expenses_controller.rb
+++ app/controllers/v1/expenses_controller.rb
@@ -13,15 +13,15 @@ class V1::ExpensesController < ApplicationController
     page_size = (params.dig(:page, :size) || 10).to_i
     page_cursor = (params.dig(:page, :cursor) || 0).to_i
 
-    expenses = Expense.where("id > ?", page_cursor).limit(page_size)
-    total = Expense.count
+    expenses = Current.user.expenses.where("id > ?", page_cursor).limit(page_size)
+    total = Current.user.expenses.count
 
     last_cursor = total > 0 ? [ (total / page_size) * page_size, total ].min : nil
 
     first_link = "http://localhost:3000/v1/expenses?page[cursor]=0&page[size]=#{page_size}"
     last_link = last_cursor ? "http://localhost:3000/v1/expenses?page[cursor]=#{last_cursor}&page[size]=#{page_size}" : nil
-    prev_link = expenses.first.id != 1 ? "http://localhost:3000/v1/expenses?page[cursor]=#{expenses.first.id - 1}&page[size]=#{page_size}" : nil
-    next_link = expenses.last.id != total ? "http://localhost:3000/v1/expenses?page[cursor]=#{expenses.last.id}&page[size]=#{page_size}" : nil
+    prev_link = !expenses.empty? && expenses.first.id != 1 ? "http://localhost:3000/v1/expenses?page[cursor]=#{expenses.first.id - 1}&page[size]=#{page_size}" : nil
+    next_link = !expenses.empty? && expenses.last.id != total ? "http://localhost:3000/v1/expenses?page[cursor]=#{expenses.last.id}&page[size]=#{page_size}" : nil
 
     render json: ExpenseSerializer.new(expenses, {
       links: {
@@ -36,6 +36,8 @@ class V1::ExpensesController < ApplicationController
   private
 
   def expense_params
-    params.require(:expense).permit(:item, :amount_in_cents, :currency, :purchased_at)
+    permitted_params = params.require(:expense).permit(:item, :amount, :currency, :purchased_at)
+    permitted_params[:amount_in_cents] = (permitted_params[:amount].to_f * 100).to_i
+    permitted_params.except(:amount)
   end
 end
