58eaeb2c18481df36442fa13381ab3a9e7a9b797
lib <lib.martinito@pm.me>
2024-09-30 23:13:51 +0800

    fix: send html instead of rendering

--- app/controllers/api_docs_controller.rb
+++ app/controllers/api_docs_controller.rb
@@ -1,6 +1,8 @@
 class ApiDocsController < ApplicationController
+  skip_before_action :authenticate_request
+
   def index
     version = params[:version].presence_in(%w[v1]) || raise(ActionController::BadRequest)
-    render file: "public/api/#{version}/docs.html"
+    send_file("public/api/#{version}/docs.html", type: "text/html", disposition: "inline")
   end
 end
